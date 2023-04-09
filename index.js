61925fd53f0d3e3ff3e470ba8a1575e03884c48e
MiguelLarios <mrlarios5@gmail.com>
2023-04-09 17:48:37 -0400

    complete lab

---
+++ index.js
@@ -0,0 +1,15 @@
+function receivesAFunction (callback) {
+    callback();
+}
+
+function returnsANamedFunction (returnFunction) {
+    return function returnsANamedFunction(){
+        
+    }
+} 
+
+
+function returnsAnAnonymousFunction(){
+    return function () {};
+}
+
